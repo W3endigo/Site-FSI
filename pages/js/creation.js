e27e519d48b4a40f92b50c277d953329456983e1
W3endigo <71370309+W3endigo@users.noreply.github.com>
2022-06-21 15:24:36 +0200

    oupla

--- pages/js/creation.js
+++ pages/js/creation.js
@@ -1,18 +1,40 @@
 // Création du match
 $("#formulaire").submit((event) => {
     event.preventDefault();
-    
-    alert("samarch");
+    if($("#motDePasse").val() == $("#motDePasse2").val()){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://127.0.0.1/Site-FSI/pages/php/request.php/joueur?email="+$('#email').val()+"&mdp="+$('#motDePasse').val()+"&prenom="+$('#prenom').val()+"&nom="+$('#nom').val()+"&date_naissance="+$('#anniversaire').val()+"&photo="+$('#image_selected').val()+"&code_insee_ville="+$('#ville').val()+"&frequence="+$('#frequence').val());
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1/site-FSI/pages/html/inscription.html");
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+               
+                var validite = xhr.responseText;
+                
+            }
+            if(validite == "true"){
+
+                window.location.href = "http://127.0.0.1/site-FSI/pages/html/profil.html?email="+$('#email').val();
+
+            }else{
+                
+                console.log("probleme avec la requête : " + validite);
+                document.getElementById("email").borderColor="#E30613";
+                document.getElementById("mauvaisEmail").style.marginBottom = "0px";
+                document.getElementById("mauvaisEmail").style.color="#E30613";
+                document.getElementById("mauvaisEmail").style.display="flex";
+
+            }
+        }
+    }else{
+        console.log("Mots de passe différents");
+        document.getElementById("motDePasse2").borderColor="#E30613";
+        document.getElementById("mauvaisMdp").style.marginBottom = "0px";
+        document.getElementById("mauvaisMdp").style.color="#E30613";
+        document.getElementById("mauvaisMdp").style.display="flex";
+    }
+    xhr.send();
     
 });
-
-
-
-
-
-
-
-
 
 // * Cette fonction permet de récupérer le nom des villes via requête AJAX et de les afficher dans un select.
 function getVille(){
@@ -33,3 +55,13 @@ function getVille(){
     }
     xhr.send();
 }
+
+function goHome(){
+    let paramString = window.location.href.split('?')[1];
+    let queryString = new URLSearchParams(paramString);
+    if(queryString.get('email') != null){
+        window.location.href = "../../index.html?email="+queryString.get('email');
+    }else{
+        window.location.href = "../../index.html";
+    }
+}   
