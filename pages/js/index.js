b822fbff6ce2ee0a4fb1648cf9f8a8dffe300e4a
Paum2tair <antoine.noel@isen-ouest.yncrea.fr>
2022-06-22 13:41:22 +0200

    oui

--- pages/js/index.js
+++ pages/js/index.js
@@ -108,8 +108,35 @@ function goConnexionProfil(){
 }  
 
 
+
+// * Cette fonction permet de récupérer le nom des villes via requête AJAX et de les afficher dans un select.
+function getVille(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1/Site-FSI/pages/php/request.php/ville");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1/site-FSI/pages/html/inscription.html");
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            var villes = JSON.parse(xhr.responseText);
+            var select = document.getElementById("villes");
+            for(var i = 0; i < villes.length; i++){
+                var option = document.createElement("option");
+                option.value = villes[i].code_insee_ville;
+                option.text = villes[i].nom_ville;
+                select.appendChild(option);
+            }
+        }
+    }
+    xhr.send();
+}
+
+
+
+
+
+
 // * permet de naviguer entre les filtres
 
+//
 function selectFiltre(filtre){
     switch(filtre){
 
