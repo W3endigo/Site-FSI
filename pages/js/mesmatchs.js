6a33e7ef91c503bddb0781ce76cd526c5974e049
W3endigo <71370309+W3endigo@users.noreply.github.com>
2022-06-23 16:02:28 +0200

    commentaire

--- pages/js/mesmatchs.js
+++ pages/js/mesmatchs.js
@@ -1,5 +1,4 @@
-// * Permet de cliquer sur un match 
-
+// * Permet de cliquer sur un match pour se rendre sur sa page
 function ouvrir_match(){
 
     let paramString = window.location.href.split('?')[1];
@@ -11,8 +10,7 @@ function ouvrir_match(){
     }
 }
 
-// * Fonction permettant de revenir au menu en restant connecté
-
+// * Fonction permettant de revenir au menu en restant connecté (si l'on est connecté)
 function goHome(){
     let paramString = window.location.href.split('?')[1];
     let queryString = new URLSearchParams(paramString);
@@ -23,21 +21,23 @@ function goHome(){
     }
 }
 
-
-
-
- // * Cette fonction permet de récupérer les informations du joueur via requête AJAX et de les afficher dans le profil.
+// * Cette fonction permet de récupérer les informations du joueur via requête AJAX et de les afficher dans le profil.
  function getJoueur(){
+
+    // * Préparation à la récupération des données de l'URL.
     let paramString = window.location.href.split('?')[1];
     let queryString = new URLSearchParams(paramString);
+
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "../php/request.php/joueur?email="+queryString.get('email'));
     xhr.setRequestHeader("Access-Control-Allow-Origin", "../html/inscription.html");
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
+
             var joueur = JSON.parse(xhr.responseText);
             document.getElementById("user_image").src = joueur.photo;        
         }
     }
+    // * Envoi de la requête.
     xhr.send();
 }
